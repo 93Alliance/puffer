4baeb1a07ab96b48b68f39396089d7d879e847ed
vincent <fei.wang@sci-jet.com>
2022-11-15 19:36:29 +0800

    创建项目

---
+++ frontend/src/app/linkview/linkview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-linkview',
+  templateUrl: './linkview.component.html',
+  styleUrls: ['./linkview.component.scss']
+})
+export class LinkviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/linkview/linkview.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LinkviewComponent } from './linkview.component';
+
+
+
+@NgModule({
+  declarations: [
+    LinkviewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LinkviewModule { }
